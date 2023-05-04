005e3dc8992e6bbcc3da520e4340525dd9208833
TanvirAhmed41 <santoahmed225@gmail.com>
2023-05-05 03:33:41 +0600

    Showm single data api

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ app.get('/chefs',(req,res)=>{
     res.send(chefs)
 })
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get("/chef/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const singleData = chefs.find((chef) => chef.id === id);
+    res.send(singleData);
+  });
+
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
